df8278531f384842123a95e5a9c17f01b134e5ec
Emilio Andere Lopez <65134108+EmilioAndere@users.noreply.github.com>
2021-07-20 10:01:46 -0500

    new index

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+if($_GET){
+    $controller = $_GET['controller'].'Controller';
+    $action = $_GET['action'];
+
+    require './Controllers/'.$controller.'.php';
+
+    $instance = new $controller();
+
+    $instance->{$action}();
+}
